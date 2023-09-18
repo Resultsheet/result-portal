0bb5aae5bcec346bfdf5cd8a678804e75f83faf2
Janith Madarasinghe <janithpm9991@gmail.com>
2023-09-18 15:37:40 +0530

    dummy styling added

--- src/pages/signin/SignInPage.tsx
+++ src/pages/signin/SignInPage.tsx
@@ -1,18 +1,32 @@
 import { Box, Button, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 
+import classes from "../../styles/signin/signinPage.style"
+
 function SignInPage() {
     const navigate = useNavigate()
     return (
         <Box>
-            <Typography variant="h4">Sign In Page</Typography>
+            <Typography
+                variant="h4"
+                sx={classes.signInBoxTitle}
+            >Sign In Page</Typography>
             <Button
                 variant="contained"
                 color="success"
+                sx={classes.signInBoxButton}
                 onClick={() => {
                     navigate('/auth/signup')
                 }}
             >Sign Up</Button>
+
+            <Typography
+                sx={{
+                    color: 'red',
+                    fontSize: '1.5rem'
+                }}
+
+            >Inline css</Typography>
         </Box>
     )
 }
---
+++ src/styles/signin/signinPage.style.ts
@@ -0,0 +1,26 @@
+const classes = {
+    signInBox: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    signInBoxTitle: {
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        marginBottom: '1rem',
+    },
+    signInBoxInput: {
+        width: '100%',
+        height: '2rem',
+        marginBottom: '1rem',
+    },
+    signInBoxButton: {
+        width: '100%',
+        height: '2rem',
+        marginBottom: '1rem',
+    },
+}
+
+export default classes
