964267b27c6c6f98572d582aab6045ed91e161ef
Janith Madarasinghe <janithpm9991@gmail.com>
2023-09-18 15:41:46 +0530

    sass added

--- src/pages/signin/SignInPage.tsx
+++ src/pages/signin/SignInPage.tsx
@@ -3,13 +3,17 @@ import { useNavigate } from "react-router-dom"
 
 import classes from "../../styles/signin/signinPage.style"
 
+import '../../scss/signin.scss'
+
 function SignInPage() {
     const navigate = useNavigate()
     return (
-        <Box>
+        <Box
+            className="signInBox" // sass method eka
+        >
             <Typography
                 variant="h4"
-                sx={classes.signInBoxTitle}
+                sx={classes.signInBoxTitle} // mui style object method eka
             >Sign In Page</Typography>
             <Button
                 variant="contained"
@@ -24,7 +28,7 @@ function SignInPage() {
                 sx={{
                     color: 'red',
                     fontSize: '1.5rem'
-                }}
+                }} //inline css method eka
 
             >Inline css</Typography>
         </Box>
